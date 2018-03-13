13faaf3362c83a066b2cf92f1d65bc84ea4724c3
guang16 <31829031+guang16@users.noreply.github.com>
2018-03-13 08:31:01 -0500

    final project
    final project

---
+++ ChatOL/app/src/main/java/com/example/glennguan/chatol/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.glennguan.chatol;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.home_page_l);
+        } else{
+            setContentView(R.layout.home_page_p);
+        }
+        Log.i("MyProgram","onCreate called");
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
+}
